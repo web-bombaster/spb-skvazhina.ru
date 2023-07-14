00b715358f3ca1c5e32562a39e03a5f956bbdd74
web-bombaster <skorpion_05@inbox.ru>
2023-07-14 17:57:31 +0300

    4 секции без адаптива

--- src/js/common.js
+++ src/js/common.js
@@ -1,6 +1,7 @@
 
 document.addEventListener(
 	"DOMContentLoaded", () => {
+		// include './myscripts/menu.js'
 		// -include './myscripts/preloader.js'
 		// -include './myscripts/burger.js'
 		// -include './myscripts/form.js'
