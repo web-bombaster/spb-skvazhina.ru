250e7d9a7d4fc5a78672801caaebff5775dd7e5c
web-bombaster <skorpion_05@inbox.ru>
2023-07-18 13:02:53 +0300

    панель для мобильного меню

--- src/js/myscripts/mobile-menu.js
+++ src/js/myscripts/mobile-menu.js
@@ -1,53 +1,78 @@
 if (document.querySelector('.jsMobileMenuBtnToggle')) {
 
-    const menuBtn = document.querySelector('.jsMobileMenuBtnToggle');
-    // console.log(menuBtn);
-
-    // Показать / скрыть мобильное меню
-    function menuToggle() {
-        const body = document.querySelector('body');
+    let menuInit = function () {
+        const menuBtn = document.querySelector('.jsMobileMenuBtnToggle');
         const menu = document.querySelector('.mobile-menu');
-
-        if (!menuBtn.classList.contains('toggle')) {
-            menu.classList.add('toggle');
-            body.classList.add('toggle');
-        } else {
-            menu.classList.remove('toggle');
-            body.classList.remove('toggle');
-        }
-
-        window.addEventListener('resize', function () {
+        const body = document.querySelector('body');
+        // const menuLink = document.querySelectorAll('.menu-link'); // для  меню по секциям
+    
+        // Показать / скрыть мобильное меню - toggle
+        function menuToggle() {
+            if (menuBtn.classList.contains('toggle')) {
+                menu.classList.add('toggle');
+                body.classList.add('toggle');
+            } else {
+                menu.classList.remove('toggle');
+                body.classList.remove('toggle');
+            };
+        };
+    
+        // Определяем высоту мобильного меню и размещаем под шапкой
+        function positionMobileMenu() {
+            const heightViewport = document.documentElement.clientHeight;
+            const heightHeader = document.querySelector('.header').offsetHeight;
+            const heightMenuOverlay = heightViewport - heightHeader;
+            // let posTop = window.pageYOffset;
+            let posTop = window.scrollY;
+    
+            const menuActive = document.querySelector('.mobile-menu.toggle');
+    
+            if (menuActive) {
+                menuActive.style.height = heightMenuOverlay + 'px';
+                menuActive.style.top = posTop + heightHeader + 'px';
+            };
+        };
+    
+        // Закрыть мобильное меню
+        function closeMenu() {
             menuBtn.classList.remove('toggle');
             menu.classList.remove('toggle');
             body.classList.remove('toggle');
+        };
+
+        // Закрываем мобильное меню по клику вне его
+        function closeMobileMenu() {
+            document.addEventListener("click", function(e) {
+                const target = e.target;
+                const its_menu = target == menu || menu.contains(target);
+                const its_btnMenu = target == menuBtn;
+    
+                if (!its_menu && !its_btnMenu) {
+                    closeMenu();
+                };
+            });
+        };
+    
+        // Закрыть мобильное меню при resize
+        window.addEventListener('resize', function () {
+            closeMenu();
         }, true);
+    
+        // Закрываем меню (для меню по секциям)
+        // menuLink.forEach(element => {
+        //     element.addEventListener("click", closeMenu);
+        // });
+    
+        // Показать / скрыть мобильное меню
+        let menuLaunch = function() {
+            menuToggle();
+            positionMobileMenu();
+            closeMobileMenu();
+        };
 
-        // heightMenuOverlay(); // меняем высоту оверлея меню при ресайзе
-    }
-
-    menuBtn.addEventListener("click", menuToggle);
-
-    // Закрываем мобильное меню по клику вне его
-    function closeMobileMenu() {
-        const btnMenu = document.querySelector('.jsMobileMenuBtnToggle');
-        const body = document.querySelector('body');
-        const menu = document.querySelector('.mobile-menu');
-
-        document.addEventListener("click", function (e) {
-            const target = e.target;
-            const its_menu = target == menu || menu.contains(target);
-            const its_btnMenu = target == btnMenu;
-
-            if (!its_menu && !its_btnMenu) {
-                menuBtn.classList.remove('toggle');
-                menu.classList.remove('toggle');
-                body.classList.remove('toggle');
-            }
-        });
+        menuBtn.addEventListener("click", menuLaunch);
     };
 
-    closeMobileMenu();
+    menuInit();
 
 };
-
-
